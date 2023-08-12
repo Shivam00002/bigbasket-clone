f8d166a30ab95e9c8490aa44d3fac560c829ecab
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-08-12 23:53:09 +0530

    Update ListHeader.jsx

--- client/src/Component/ListHeader.jsx
+++ client/src/Component/ListHeader.jsx
@@ -2,6 +2,7 @@ import { Text } from "@chakra-ui/react";
 
 export const ListHeader = ({ children }) => {
   return (
+
     <Text color={"#84c225"}  fontSize={"lg"} mb={2}>
       {children}
     </Text>
