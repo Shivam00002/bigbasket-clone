b359682cf8c9e578ac7b2f32f51f12d0d3c1da0e
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-09-08 23:56:15 +0530

    Update cart.actionType.js

--- client/src/Redux/Cart/cart.actionType.js
+++ client/src/Redux/Cart/cart.actionType.js
@@ -3,9 +3,6 @@ export const CART_LOADING = "cart/loading";
 export const CART_ERROR = "cart/error"
 export const CART_CLEAR_ERROR = "cart/clearError";
 
-
-
-
 export const CART_INC_COUNT = "cart/inc";
 export const CART_DEC_COUNT = "cart/dec";
 export const CART_RESET = "cart/reset"
