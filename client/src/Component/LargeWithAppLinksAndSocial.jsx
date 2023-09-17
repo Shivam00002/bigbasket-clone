2a93f2213989f0be1f1c0aaba26a38261d1b05db
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-09-17 23:12:37 +0530

    Update LargeWithAppLinksAndSocial.jsx

--- client/src/Component/LargeWithAppLinksAndSocial.jsx
+++ client/src/Component/LargeWithAppLinksAndSocial.jsx
@@ -4,6 +4,7 @@ import { SocialButton } from './SocialButton';
 import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 
+
 export default function LargeWithAppLinksAndSocial() {
   return (
     <Box
